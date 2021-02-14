07dd882864d148c7dded24a2c090ee71ac0ca846
RobNL <robbert.hessing@outlook.com>
2021-02-14 21:07:08 +0100

    Update docker-entrypoint.sh
    Log all to stdout, removed debugging option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,12 @@ config_file="/etc/supervisord.conf"
 # Set env controlled variables
 COMMAND=${COMMAND:-php /var/www/apps/laravel/artisan queue:work --sleep=3 --tries=3 --daemon}
 PROCESSES=${PROCESSES:-4}
-DEBUG=${DEBUG:-false}
 
 # Supervisord config file contents
 cat << EOF > $config_file
 [supervisord]
 nodaemon=true
+user=root
 [supervisorctl]
 [rpcinterface:supervisor]
 supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
@@ -24,13 +24,9 @@ autostart=false
 autorestart=false
 numprocs=$PROCESSES
 redirect_stderr=true
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes = 0
 EOF
 
-if [ "${DEBUG}" != "true" ]
-then
-  echo "Supervisord configuration completed, starting supervisord"
-  /usr/bin/supervisord -n -c /etc/supervisord.conf
-else
-  echo "Debug environment detected, not starting supervisord. Have fun debugging!"
-  eval /bin/sh
-fi
+echo "Supervisord configuration completed, starting supervisord"
+/usr/bin/supervisord -n -c /etc/supervisord.conf
